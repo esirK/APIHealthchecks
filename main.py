40b550f2f829e24d7a94eb7fe3c0334003231cc0
esir <esirkings@gmail.com>
2020-08-25 14:59:30 +0300

    Set request User-Agent since some APIs don't accept requests from 'non-browsers'.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import json
 import requests
 
+session = requests.Session()
+session.headers.update({'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"})
+
 def run():
     with open("apis.json", 'r') as apis:
         apis_data = json.load(apis)
         for api in apis_data:
-            api_response = requests.get(api)
+            api_response = session.get(api)
             if api_response.ok:
                 # More customisation e.g checks of the response can be done here e.g
                 # api_response.json()['success'] == 'True'
