ca30bd4cb4a58deefa7e2898c6c4a1ca533bdbd6
esir <esirkings@gmail.com>
2020-08-25 10:31:51 +0300

    Prepare project for AWS lambda deployment

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import json
 import requests
 
-
-with open("apis.json", 'r') as apis:
-    apis_data = json.load(apis)
-    for api in apis_data:
-        api_response = requests.get(api)
-        if api_response.ok:
-            # More customisation e.g checks of the response can be done here e.g
-            # api_response.json()['success'] == 'True'
-            # Ping Healthchecks
-            requests.get(apis_data[api])
+def run():
+    with open("apis.json", 'r') as apis:
+        apis_data = json.load(apis)
+        for api in apis_data:
+            api_response = requests.get(api)
+            if api_response.ok:
+                # More customisation e.g checks of the response can be done here e.g
+                # api_response.json()['success'] == 'True'
+                # Ping Healthchecks
+                requests.get(apis_data[api])
